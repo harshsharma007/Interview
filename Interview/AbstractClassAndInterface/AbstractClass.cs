22732fb6194724088953af7fcb22d474bdeed68a
Harsh Sharma <harshsharmahs@gmail.com>
2020-01-28 09:59:13 +0530

    Design Pattern

--- Interview/AbstractClassAndInterface/AbstractClass.cs
+++ Interview/AbstractClassAndInterface/AbstractClass.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Interview
 {
+    //Why we need an abstract class?
+    //Because using an abstract class will restrict a user to not create an object of an abstract class. Otherwise, this would not be a case in other concrete class in
+    //which an object of a class can be created and may result in unexpected behaviors because it may contain some functions that need to be declared in the inherited
+    //class.
     abstract class AbstractClass
     {
         //A property with private set
@@ -21,19 +25,34 @@ namespace Interview
 
     public abstract class NewAbstract
     {
-        //We can have an abstract class without abstract method, as both are independent concepts. Declaring a class abstract means that it can not be 
-        //instantiated on its own and can only be subclassed. Declaring a method abstract means that Method will be defined in the subclass.
+        //We can have an abstract class without abstract method, as both are independent concepts. Declaring a class abstract means that it can not be instantiated
+        //on its own and can only be subclassed. Declaring a method abstract means that Method will be defined in the subclass.
+    }
+
+    abstract class NewAbstractClass
+    {
+        //By default, if no access modifier is applied on a constructor then it would be a private constructor and a class cannot inherit the whole class due to its
+        //protection level.
+        //Error: 'ClassName' is inaccessible due to its protection level.
+        public NewAbstractClass()
+        {
+            Console.WriteLine("In abstract");
+        }
     }
 
     class ParentAbstract : AbstractClass
     {
-        //Now the ParentAbstract class has to implement abstract method Method1, otherwise it will throw an exception.
+        //Now the ParentAbstract class has to implement abstract method Method1 otherwise, it will throw an exception.
         //Error: 'ParentAbstract' does not implement inherited abstract method 'AbstractClass.Method1()'.
         public override void Method1()
         {
             
         }
 
+        //To invoke the parameterized constructor of an abstract class below is the code. It can be invoked using the base keyword. Supply an argument to the base class
+        //constructor.
+        //So you don't necessarily need to pass any information to the derived class constructor, but the derived class constructor must pass information to the base
+        //class constructor if that only exposes a parameterized constructor.
         public ParentAbstract() : base("Test")
         {
 
@@ -45,5 +64,16 @@ namespace Interview
         //An empty abstract class can be inherited.
         //But a class cannot inherit multiple abstract class. It will throw a compile-time error.
         //Error: Class '' cannot have multiple base classes.
+    }
+
+    class ParentAbstractClass : NewAbstractClass
+    {
+        //A class cannot be instantiated if it contains a private constructor.
+        //If a class inherits other class and both classes consist of a constructor, then the constructor of a parent class will be called first and after that child class
+        //constructor will be called.
+        public ParentAbstractClass()
+        {
+            Console.WriteLine("In Child Class");
+        }
     }
 }
---
+++ Interview/DesignPatterns/SingletonDesignPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.DesignPatterns
+{
+    class SingletonDesignPattern
+    {
+        //What is Singleton Design Pattern?
+        //1. It ensures a class has only one instance and provides a global point of access to it.
+        //2. A singleton is a class that only allows a single instance of itself to be created and usually gives simple access to that instance.
+        //3. Most commonly, singletons don't allow any parameters to be specified when creating the instance, since a second request of an instance with a different
+        //parameter could be problematic. (If the same instance should be accessed for all requests with the same parameter then the factory pattern is more
+        //appropriate.)
+
+        //There are various ways to implement Singleton Pattern in C#. The following are the common characteristics of a Singleton Pattern.
+        //1. A single constructor that is private and parameterless.
+        //2. Class is sealed.
+        //3. The static variable that holds a reference to the single created instance, if any.
+        //4. A public static means of getting the reference to the single created instance, creating one if necessary.
+
+        //Advantages of Singleton Pattern are:
+        //1. Singleton pattern can implement interfaces.
+        //2. It can also be inherited from other classes.
+        //3. It can be lazy loaded.
+        //4. It has Static Initialization.
+        //5. It can be extended into a factory pattern.
+        //6. It helps to hide dependencies.
+        //7. It provides a single point of access to a particular instance, so it is easy to maintain.
+
+        //Disadvantages of Singleton Pattern are:
+        //1. Unit testing is more difficult (because it introduces a global state into an application).
+        //2. This pattern reduces the potential for parallelism within a program, because to access the singleton in a multi-threaded system, an object must be serialized.
+
+        //Singleton class vs Static methods
+        //1. A Static class cannot be extended whereas a singleton class can be extended.
+        //2. A Static class can still have instances (unwanted instances) whereas a singleton class prevents it.
+    }
+}
