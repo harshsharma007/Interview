19b4f92a92d7f03615b64675f848aee283edb1cc
Harsh Sharma <harshsharmahs@gmail.com>
2020-10-20 11:26:26 +0530

    Interview

--- Interview/Basic/Exercise.cs
+++ Interview/Basic/Exercise.cs
@@ -22,5 +22,90 @@ namespace Interview.Basic
             Console.WriteLine("{0} {1}", "My DOB is", birthDate);
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "My address is", addressLine1, addressLine2, city, state, zip.ToString(), country);
         }
+
+        public void DecisionAndRepetition()
+        {
+            int condition1;
+            int condition2;
+            int switchCondition;
+
+            condition1 = 1;
+            if (condition1 == 1)
+            {
+                Console.WriteLine("Comparison evaluated to true.");
+            }
+
+            Console.WriteLine("This executes after the if, regardless of what has taken place.");
+
+            condition1 = 3;
+            if (condition1 == 1)
+            {
+                Console.WriteLine("Comparison evaluated to true.");
+            }
+            else
+            {
+                Console.WriteLine("Comparison evaluated to false.");
+            }
+
+            condition1 = 1;
+            condition2 = 2;
+            if (condition1 == 1)
+            {
+                Console.WriteLine("Condition1 is true");
+            }
+            else if (condition2 == 2)
+            {
+                Console.WriteLine("Condition2 is true");
+            }
+            else
+            {
+                Console.WriteLine("Neither condition is true");
+            }
+
+            switchCondition = 3;
+            switch (switchCondition)
+            {
+                case 1:
+                    Console.WriteLine("Value is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Value is 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Value is 3");
+                    break;
+                default:
+                    Console.WriteLine("Value is " + switchCondition);
+                    break;
+            }
+
+            int whileCounter = 0, doCounter = 0;
+
+            Console.WriteLine("For Loop");
+            Console.WriteLine("");
+
+            for (int forCounter = 0; forCounter < 10; forCounter++)
+            {
+                Console.WriteLine("forCounter is at {0}", forCounter);
+            }
+
+            Console.WriteLine("While Loop");
+            Console.WriteLine("");
+
+            while (whileCounter < 5)
+            {
+                Console.WriteLine("Whilecounter is at " + whileCounter);
+                whileCounter++;
+            }
+
+            Console.WriteLine("Do-While Loop");
+            Console.WriteLine("");
+            
+            do
+            {
+                Console.WriteLine("doCounter is at " + doCounter);
+                doCounter++;
+            } while (doCounter < 5);
+        }
     }
 }
