544a94016ace6833e18b20589d14d934e6af712f
Harsh Sharma <harshsharmahs@gmail.com>
2020-10-26 11:27:11 +0530

    Interview

---
+++ Interview/ComplexDataStructure/ExtendingStructs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.ComplexDataStructure
+{
+    class ExtendingStructs
+    {
+        /*
+            Extending Structs
+            In order to go beyond a simple struct, you can extend it by adding properties and indexers. This section discusses using properties and indexers in your
+            struct. Again, if you find yourself going to this extent, evaluate your use of structs against class files.
+            
+            Creating Properties
+            In Visual C#, a property is a programming construct that enables client code to get or set the value of private fields within a struct or a class. To
+            consumers of your struct or class, the property behaves like a public field. Within your struct or class, the property is implemented by using accessors,
+            which are a special type of method. A property can include one or both of the following:
+            
+            1. A get accessor to provide read access to a field.
+            2. A set accessor to provide write access to a field.
+            
+            The following example shows how to implement a property in a struct:
+        */
+    }
+}
