b8e8699856cebf0569dce6c5096b3c3f374aad2a
Harsh Sharma <harshsharmahs@gmail.com>
2020-11-09 00:08:14 +0530

    Interview

---
+++ Interview/ObjectOrientedProgramming/InterfacePolymorphism.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.ObjectOrientedProgramming
+{
+    class InterfacePolymorphism
+    {
+        /*
+            Interface Polymorphism
+            As it relates to interfaces, polymorphism states that you can represent an instance of a class as an instance of any interface that the class implements.
+            Interface polymorphism can help to increase the flexibility and modularity of your code. Suppose you have several classes that implement an IBeverage
+            interface, such as Coffee, Tea, Juice and so on. You can write code that works with any of these classes as instances of IBeverage, without knowing any
+            details of the implementing class. 
+        */
+    }
+}
