16bf2f0082964edf869f15f8973fcc785e5d1c1f
Harsh Sharma <harshsharmahs@gmail.com>
2020-11-11 10:19:25 +0530

    Interview

---
+++ Interview/Collections/IntroducingCollections.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.Collections
+{
+    class IntroducingCollections
+    {
+        /*
+            Introducing Collections
+            When you create multiple items of the same type, regardless of whether they are integers, strings or a custom type such as Coffee, you need a way of
+            managing the items as a set. You need to be able to count the number of items in the set, add items to or remove items from the set, and iterate through
+            the set one time at a time. To do this, you can use a collection.
+            
+            Collections are an essential tool for managing multiple items. They are also central to developing graphical applications. Controls such as drop-down list
+            boxes and menus are typically data-bound to collections.
+            
+            Choosing Collections
+            All collection classes share various common characteristics. To manage a collection of items, you must be able to:
+            
+            1. Add items to the collection
+            2. Remove items from the collection
+            3. Retrieve specific items from the collection
+            4. Count the number of items in the collection
+            5. Iterate through the items in the collection, one item at a time.
+            
+            Every collection class in Visual C# provides methods and properties that support these core operations. Beyond these operations, however, you will want
+            to manage collections in different ways depending on the specific requirements of your application. Collection classes in Visual C# fall into the following
+            broad categories:
+            
+            1. List classes store linear collections of items. You can think of a list class as a one-dimensional array that dynamically expands as you add items.
+               For example, you might use a list class to maintain a list of available beverages at your coffee shop.
+            
+            2. Dictionary classes store a collection of key/value pairs.
+        */
+    }
+}
