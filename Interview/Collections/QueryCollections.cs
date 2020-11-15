f711f63aeece38e7d541e8bc28fb025e8c51fa66
Harsh Sharma <harshsharmahs@gmail.com>
2020-11-16 00:06:31 +0530

    Interview

---
+++ Interview/Collections/QueryCollections.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.Collections
+{
+    class QueryCollections
+    {
+        /*
+            Query Collections with Predicates and Lambda Expressions
+            Some collections in the .NET Framework do not support the use of array notation for accessing items in the collection. These collections provide the Find
+            method for locating items in the collection. The Find method requires a predicate to be used as the criteria for its search. In this case,
+        */
+    }
+}
