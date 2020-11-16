a8337423038e5c2a46358b3358df4febe5448298
Harsh Sharma <harshsharmahs@gmail.com>
2020-11-16 23:39:38 +0530

    Interview

---
+++ Interview/Collections/CreatingAndUsingGenericClasses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.Collections
+{
+    class CreatingAndUsingGenericClasses
+    {
+        /*
+            Creating and Using Generic Classes
+            Generic classes work by including a type parameter, T, in the class or interface declaration. You do not need to specify the type of T until you instantiate the class.
+            To create a generic class, you need to:
+            
+            1. Add the type parameter T in angle brackets after the class name.
+            2. Use the type parameter T in place of type names in your class members.
+            
+            The following example shows how to create a generic class:
+            
+            // Creating a Generic Class
+            pubilc class CustomList<T>
+            {
+                
+            }
+        */
+    }
+}
