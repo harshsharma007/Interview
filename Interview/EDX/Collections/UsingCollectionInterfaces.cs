7f47643ab9a9e8481e94246f80466a254579dc43
Harsh Sharma <harshsharmahs@gmail.com>
2020-11-24 21:00:48 +0530

    Collections

--- Interview/EDX/Collections/UsingCollectionInterfaces.cs
+++ Interview/EDX/Collections/UsingCollectionInterfaces.cs
@@ -24,6 +24,51 @@ namespace Interview.EDX.Collections
             interface, it exposes all the members of the parent interface. In other words, if you implement IEnumerable<T>, you also need to implement IEnumerable.
             
             The ICollection<T> Interface
+            The ICollection<T> interface defines the basic functionality that is common to all generic collections. The interface inherits from IEnumerable<T>, which
+            means that if you want to implement ICollection<T>, you must also implement the members defined by IEnumerable<T> and IEnumerable.
+            
+            The ICollection<T> interface defines the following methods:
+            
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Name        |       Description                                                                 |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Add         |       Adds an item of type T to the collection.                                   |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Clear       |       Removes all items from the collection.                                      |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Contains    |       Indicates whether the collection contains a specific value.                 |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       CopyTo      |       Copies the items in the collection to an array.                             |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Remove      |       Removes a specific object from the collection.                              |
+            +-------------------+-----------------------------------------------------------------------------------+
+            
+            The ICollection<T> interface defines the following properties.
+            
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Name        |       Description                                                                 |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Count       |       Gets the number of items in the collection.                                 |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |    IsReadOnly     |       Indicates whether the collection is read-only.                              |
+            +-------------------+-----------------------------------------------------------------------------------+
+            
+            The IList<T> Interface
+            The IList<T> interface defines the core functionality for generic list classes. You should implement the interface if you are defining a linear collection
+            of single values. In addition to the members it inherits from ICollection<T>, the IList<T> interface defines methods and properties that enable you to use
+            indexers to work with the items in the collection. For example, if you create a list named myList, you can use myList[0] to access the first item in the
+            collection.
+            
+            The IList<T> interface defines the following methods:
+            
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Name        |       Description                                                                 |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |       Insert      |       Inserts an item into collection at the specified index.                     |
+            +-------------------+-----------------------------------------------------------------------------------+
+            |      RemoveAt     |       Removes the item at the specified index from the collection.                |
+            +-------------------+-----------------------------------------------------------------------------------+
+            
             
         */
     }
