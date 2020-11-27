bc4e79fb957ba0e852e19dffd3294f48b5a6b076
Harsh Sharma <harshsharmahs@gmail.com>
2020-11-27 20:01:28 +0530

    Interview

---
+++ Interview/EDX/GenericCollections/GenericsDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.EDX.Collections
+{
+    class GenericsDemo
+    {
+        public void GenericsMethod()
+        {
+            #region GenericCollections
+            // Create string objects
+            string s1 = "Latte";
+            string s2 = "Espresso";
+            string s3 = "Americano";
+            string s4 = "Cappuccino";
+            string s5 = "Mocha";
+
+            // Add the items to a strongly-typed collection List<T>
+            var coffeeBeverages = new List<string>();
+            coffeeBeverages.Add(s1);
+            coffeeBeverages.Add(s2);
+            coffeeBeverages.Add(s3);
+            coffeeBeverages.Add(s4);
+            coffeeBeverages.Add(s5);
+            #endregion
+        }
+    }
+}
