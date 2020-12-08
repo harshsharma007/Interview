c59b0af4fd02aa86a5a16ca4b6f4c9ebf36774ee
Harsh Sharma <harshsharmahs@gmail.com>
2020-12-08 19:42:06 +0530

    Linq

---
+++ Interview/EDX/LINQ/OrderingAndGrouping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.EDX.LINQ
+{
+    class OrderingAndGrouping
+    {
+        /*
+            Grouping Data
+            The following code example shows how to use a group clause to group the returned employees by their Job Title ID.
+        */
+    }
+}
